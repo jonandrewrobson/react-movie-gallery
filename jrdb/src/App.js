5957f9785794633afe4a237617a6451059403565
Jon Robson <jon.robson@ampf.com>
2019-03-05 10:41:21 -0600

    Set up proptypes and default prop type for movie descriptions

--- jrdb/src/App.js
+++ jrdb/src/App.js
@@ -5,7 +5,8 @@ import Movie from './Movie'
 
 const movies = [{
   id: 1,
-  title: 'Dark City'
+  title: 'Dark City',
+  description: 'This is a description'
 }, {
   id: 2,
   title: 'Get Out'
@@ -15,6 +16,8 @@ const movies = [{
 }
 ];
 
+// Iterating over movies. Array, mapping over it, taking each movie, and returning the movie component with the movies passed in. Loops over each movie and outputs a new component.
+
 class App extends Component {
   render() {
     return (
@@ -22,9 +25,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} description={movie.description} />)}
       </div>
     );
   }
--- jrdb/src/Movie.js
+++ jrdb/src/Movie.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+    static propTypes = {
+        movie: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+        }),
+        description: PropTypes.string
+    }
+
+    static defaultProps = {
+        description: 'Description not available'
+    }
+
     render() {
         return (
             <div>
-                {this.props.movie.title}
+                <h3>{this.props.movie.title}</h3>
+                <p>{this.props.description}</p>
             </div>
         )
     }
