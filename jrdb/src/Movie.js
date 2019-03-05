481de76ad1f6a9c585f7c395210a3d1a4ec48aa3
Jon Robson <jon.robson@ampf.com>
2019-03-05 12:42:47 -0600

    Set up browser router and route

--- jrdb/src/Movie.js
+++ jrdb/src/Movie.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// Function returning JSX
+// Setting up poster path with directory and size
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+
+// Function returning JSX div
 const Movie = ({ movie }) => (
   <div>
     <h3>{movie.title}</h3>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt="{movie.title}" />
   </div>
 );
 
