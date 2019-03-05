f40d44e3fc66962435ec58cf1535cd966fce3857
Jon Robson <jon.robson@ampf.com>
2019-03-05 10:11:17 -0600

    Refactored css / Created a movies array / Mapped array to the movies component / Looped over movies array and output the movie data into App

--- jrdb/src/App.js
+++ jrdb/src/App.js
@@ -1,45 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+
+const movies = [{
+  id: 1,
+  title: 'Dark City'
+}, {
+  id: 2,
+  title: 'Get Out'
+}, {
+  id: 3,
+  title: 'Men Who Stare At Goats'
+}
+];
 
 class App extends Component {
-
-  state = {
-    input: 'hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value.trim() // Trim removes spaces
-    })
-  } // This syntax allows to use this. when referring to component
-
-  submit = () => {
-    console.log(this.text.value);
-  } // Console logging the value of the input
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to using props" />
-          <h3>{this.state.input}</h3>
-          <input type="text" onChange={this.updateInput} value={this.state.input} />
-          <input type="text" ref={(input) => this.text = input} />
-          <button onClick={this.submit}>Show Value</button>
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props; // Allows to use props without having to type this.props
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
   }
 }
 
